d2eaec48507f705e776d2e4e8a1bf5de436b947d
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-03 11:20:56 -0300

    Adiciona requisito 04

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const ballContainer = document.querySelector('#ball-container');
 
+function randomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb( ${red} , ${green} , ${blue})`;
+}
+
 window.addEventListener('load', () => {
   for (let i = 0; i < 6; i += 1) {
     const ball = document.createElement('div');
     ball.classList = 'ball';
+    ball.style.backgroundColor = randomColor();
     ballContainer.appendChild(ball);
   }
 });
+
