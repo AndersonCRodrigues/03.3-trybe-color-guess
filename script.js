bbc5baac7283050d5f53f6c6983e3f9b22933423
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-03 11:11:35 -0300

    Adiciona requisito 03

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const ballContainer = document.querySelector('#ball-container');
+
+window.addEventListener('load', () => {
+  for (let i = 0; i < 6; i += 1) {
+    const ball = document.createElement('div');
+    ball.classList = 'ball';
+    ballContainer.appendChild(ball);
+  }
+});
